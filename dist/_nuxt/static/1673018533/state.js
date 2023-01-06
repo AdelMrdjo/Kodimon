e2b3c1361fcf5a586886b4f86fd858355a995db2
AdelMrdjo laptop <adel.mrdjanovic98@hotmail.com>
2023-01-06 16:23:08 +0100

    Up

---
+++ dist/_nuxt/static/1673018533/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673018533",layout:"default",error:a,state:{isGameStarted:b,attacker:1,player1:a,player2:a,winner:a,logs:[],attackInProgress:b},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,false,"\u002F"));
